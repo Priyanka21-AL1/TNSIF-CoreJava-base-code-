4e8ce13b063ad0f95900d225c7420d6ad6058d63
banga <banga@LAPTOP-9RVIGCC6>
2025-06-30 10:42:06 +0530

    New Changes

--- daytwo/src/daytwo/snehaha.java
+++ daytwo/src/daytwo/snehaha.java
@@ -4,7 +4,7 @@ public class snehaha {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("i am coder.......");
+		System.out.println("i am coder1.......");
 	}
 
 }
