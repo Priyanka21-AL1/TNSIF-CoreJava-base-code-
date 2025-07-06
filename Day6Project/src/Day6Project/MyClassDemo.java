1446f4099736c91b7c8602a8c231c46c26c7329e
banga <banga@LAPTOP-9RVIGCC6>
2025-07-06 13:44:34 +0530

    day 6

---
+++ Day6Project/src/Day6Project/MyClassDemo.java
@@ -0,0 +1,34 @@
+package Day6Project;
+
+public class MyClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//Creating first object of the class 
+		
+				
+				
+				MyClass.display();
+				
+				System.out.println();
+				
+				MyClass o1=new MyClass();
+				System.out.println(o1);
+				
+				MyClass.display();
+				
+				System.out.println();
+				
+				MyClass o2=new MyClass();
+				System.out.println(o2);
+
+				System.out.println();
+				
+				MyClass o3=new MyClass();
+				System.out.println(o3);
+
+	}
+
+}
---
+++ Day6Project/src/Day6Project/employeDemo.java
@@ -0,0 +1,13 @@
+package Day6Project;
+
+public class employeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Employe e=new Employe();
+		System.out.println(e);
+	}
+
+}
